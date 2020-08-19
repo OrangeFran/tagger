5f78486a8a42ba63c908058b047d8917dd3fe897
Finn Hediger <finn.hediger@thurweb.ch>
2020-08-19 14:52:10 +0200

    new helper function + new test

--- parser_test.go
+++ parser_test.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+    "fmt"
+    "errors"
     "testing"
 )
+
+// a helper function for evaluating test results
+func EvaluateResults(results map[string]string) error {
+    passed := true
+    failed := ""
+
+    for correct, got := range results {
+        if got != correct {
+            passed = false
+            failed = failed + fmt.Sprintf("[-] Wanted %s, got %s\n", correct, got)
+        }
+    }
+
+    if !passed {
+        return errors.New("[-] Test failed!\n" + failed)
+    }
+
+    return nil
+}
 
 func TestParseSimple(t *testing.T) {
     // create a simple formatter
@@ -13,9 +34,17 @@ func TestParseSimple(t *testing.T) {
     fm.Extract(content)
 
     // evaluate the results
-    if fm.Artist != "artist" || fm.Title != "title" {
-        t.Errorf("Simple test failed!\nWanted %s & %s, got %s & %s\n", "artist", "title", fm.Artist, fm.Title)
+    results := map[string]string{
+        "artist": fm.Artist,
+        "title": fm.Title,
     }
+
+    if err := EvaluateResults(results); err != nil {
+        t.Errorf(err.Error())
+    }
+    // if fm.Artist != "artist" || fm.Title != "title" {
+    //     t.Errorf("Simple test failed!\nWanted %s & %s, got %s & %s\n", "artist", "title", fm.Artist, fm.Title)
+    // }
 }
 
 func TestParseBeginning(t *testing.T) {
@@ -27,8 +56,14 @@ func TestParseBeginning(t *testing.T) {
     fm.Extract(content)
 
     // evaluate the results
-    if fm.Artist != "artist" || fm.Title != "title" || fm.Year != "18/10/2020" {
-        t.Errorf("Beginning test failed!\nWanted %s & %s & %s, got %s & %s & %s\n", "artist", "title", "18/10/2020", fm.Artist, fm.Title, fm.Year)
+    results := map[string]string{
+        "artist": fm.Artist,
+        "title": fm.Title,
+        "18/10/2020": fm.Year,
+    }
+
+    if err := EvaluateResults(results); err != nil {
+        t.Errorf(err.Error())
     }
 }
 
@@ -42,6 +77,20 @@ func TestParseError(t *testing.T) {
 
     // evaluate the results
     if err == nil {
-        t.Errorf("Test should have failed! It instead worked!")
+        t.Errorf("The function should have returned an error!")
+    }
+}
+
+func TestParseErrorCutOff(t *testing.T) {
+    // create a simple formatter
+    format = "music_%a - %"
+    content := "music_artist - title (18/10/2020)"
+
+    fm := Formatter {}
+    err := fm.Extract(content)
+
+    // evaluate the results
+    if err == nil {
+        t.Errorf("The function should have returned an error!")
     }
 }
